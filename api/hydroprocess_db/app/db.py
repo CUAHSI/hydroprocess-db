9fe64474ab916f3c0e1cc47e5726dc06d920a42f
Devin Cowan <dcowan@cuahsi.org>
2024-07-28 20:38:59 -0400

    more attempts at fastapi-users-db-sqlmodel

--- api/hydroprocess_db/app/db.py
+++ api/hydroprocess_db/app/db.py
@@ -1,16 +1,12 @@
-# from typing import List, Optional
-# import httpx
-from typing import AsyncGenerator
+import httpx
+from typing import AsyncGenerator, Optional, List, Tuple
+from pydantic import UUID4
 from fastapi import Depends
-from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
-# from sqlalchemy.orm import relationship, Mapped
-from sqlmodel import SQLModel
-# from sqlmodel import Field
-from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlmodel import SQLModel, Field, Relationship
 
-from fastapi_users_db_sqlmodel import SQLModelBaseUserDB, SQLModelUserDatabaseAsync
-from fastapi_users.db import SQLAlchemyBaseOAuthAccountTableUUID
+from fastapi_users_db_sqlmodel import SQLModelBaseUserDB, SQLModelUserDatabaseAsync, SQLModelBaseOAuthAccount
 # https://github.com/fastapi-users/fastapi-users/discussions/861
 # https://github.com/fastapi-users/fastapi-users-db-sqlmodel
 # https://stackoverflow.com/questions/70694787/fastapi-fastapi-users-with-database-adapter-for-sqlmodel-users-table-is-not-crea
@@ -34,38 +30,41 @@ async def create_db_and_tables():
         await conn.run_sync(SQLModel.metadata.create_all)
 
 
-class OAuthAccount(SQLAlchemyBaseOAuthAccountTableUUID):
-    # https://fastapi-users.github.io/fastapi-users/latest/configuration/oauth/?h=#configuration
-    pass
+class UserOAuth(SQLModelBaseUserDB, table=True):
+    # https://github.com/fastapi-users/fastapi-users-db-sqlmodel/blob/83980d7f20886120f4636a102ab1822b4c366f63/tests/conftest.py#L23-L25
+    __tablename__ = "user_oauth"
+    oauth_accounts: List["OAuthAccount"] = Relationship(
+        back_populates="user",
+        sa_relationship_kwargs={"lazy": "selectin", "cascade": "all, delete"},
+    )
+
+
+class OAuthAccount(SQLModelBaseOAuthAccount, table=True):
+    user_id: UUID4 = Field(foreign_key="user_oauth.id")
+    user: Optional[UserOAuth] = Relationship(back_populates="oauth_accounts")
 
 
 class User(SQLModelBaseUserDB, table=True):
-    # TODO: add OAuthAccount to User
-    # oauth_accounts: List[OAuthAccount] = Field(default_factory=list)
-    # oauth_accounts: Mapped[List[OAuthAccount]] = relationship(
-    #     "OAuthAccount", lazy="joined"
-    # )
+    name: Optional[str] = None
+    username: Optional[str] = None
+    given_name: Optional[str] = None
+    family_name: Optional[str] = None
 
-    # name: Optional[str] = None
-    # username: Optional[str] = None
-    # given_name: Optional[str] = None
-    # family_name: Optional[str] = None
+    async def update_profile(self):
+        async def get_profile(token: str) -> Tuple[str, str]:
+            async with httpx.AsyncClient() as client:
+                response = await client.get(
+                    get_settings().user_info_endpoint,
+                    headers={"Authorization": f"Bearer {token}"},
+                )
+                return response.json()
 
-    # async def update_profile(self):
-    #     async def get_profile(token: str) -> Tuple[str, str]:
-    #         async with httpx.AsyncClient() as client:
-    #             response = await client.get(
-    #                 get_settings().user_info_endpoint,
-    #                 headers={"Authorization": f"Bearer {token}"},
-    #             )
-    #             return response.json()
-
-    #     profile = await get_profile(self.oauth_accounts[0].access_token)
-    #     self.name = profile['name']
-    #     self.username = profile['preferred_username']
-    #     self.given_name = profile['given_name']
-    #     self.family_name = profile['family_name']
-    #     await self.save()
+        profile = await get_profile(self.oauth_accounts[0].access_token)
+        self.name = profile['name']
+        self.username = profile['preferred_username']
+        self.given_name = profile['given_name']
+        self.family_name = profile['family_name']
+        await self.save()
     pass
 
 
