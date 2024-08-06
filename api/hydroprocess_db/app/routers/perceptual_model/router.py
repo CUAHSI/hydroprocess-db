83b0677404b3ec8e728ca1364719822b7fab359b
Devin Cowan <dcowan@cuahsi.org>
2024-08-06 16:06:18 -0400

    use synchronous engine for model endpoints

--- api/hydroprocess_db/app/routers/perceptual_model/router.py
+++ api/hydroprocess_db/app/routers/perceptual_model/router.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import APIRouter, Depends
 from sqlmodel import select
 
-from app.db import get_async_session
+from app.db import get_session
 from app.models import (
     Citation,
     Location,
@@ -22,7 +22,7 @@ router = APIRouter()
     description="Get all perceptual models.",
     response_model=List[PerceptualModel],
 )
-async def get_perceptual_models(*, session=Depends(get_async_session)):
+def get_perceptual_models(*, session=Depends(get_session)):
     """
     Get perceptual models from the database.
 
@@ -32,7 +32,7 @@ async def get_perceptual_models(*, session=Depends(get_async_session)):
     Returns:
     - A list of perceptual models.
     """
-    models = await session.exec(select(PerceptualModel))
+    models = session.exec(select(PerceptualModel))
     return models
 
 
@@ -41,7 +41,7 @@ async def get_perceptual_models(*, session=Depends(get_async_session)):
     description="Get a perceptual model by ID.",
     response_model=PerceptualModel,
 )
-async def get_perceptual_model_by_id(model_id: int, session=Depends(get_async_session)):
+async def get_perceptual_model_by_id(model_id: int, session=Depends(get_session)):
     """
     Get a perceptual model by ID.
 
@@ -52,7 +52,7 @@ async def get_perceptual_model_by_id(model_id: int, session=Depends(get_async_se
     Returns:
     - The perceptual model with the specified ID.
     """
-    model = await session.get(PerceptualModel, model_id)
+    model = session.get(PerceptualModel, model_id)
     return model
 
 
@@ -61,7 +61,7 @@ async def get_perceptual_model_by_id(model_id: int, session=Depends(get_async_se
     description="Get the location for a perceptual model by ID.",
     response_model=Location | None,
 )
-async def get_perceptual_model_location(model_id: int, session=Depends(get_async_session)):
+async def get_perceptual_model_location(model_id: int, session=Depends(get_session)):
     """
     Get the location for a perceptual model by ID.
 
@@ -72,8 +72,8 @@ async def get_perceptual_model_location(model_id: int, session=Depends(get_async
     Returns:
     - The location for the perceptual model with the specified ID.
     """
-    model = await session.get(PerceptualModel, model_id)
-    location = await model.awaitable_attrs.location
+    model = session.get(PerceptualModel, model_id)
+    location = model.location
     return location
 
 
@@ -82,7 +82,7 @@ async def get_perceptual_model_location(model_id: int, session=Depends(get_async
     description="Get the citation for a perceptual model by ID.",
     response_model=Citation | None,
 )
-async def get_perceptual_model_citation(model_id: int, session=Depends(get_async_session)):
+async def get_perceptual_model_citation(model_id: int, session=Depends(get_session)):
     """
     Get the citation for a perceptual model by ID.
 
@@ -93,8 +93,8 @@ async def get_perceptual_model_citation(model_id: int, session=Depends(get_async
     Returns:
     - The citation for the perceptual model with the specified ID.
     """
-    model = await session.get(PerceptualModel, model_id)
-    citation = await model.awaitable_attrs.citation
+    model = session.get(PerceptualModel, model_id)
+    citation = model.citation
     return citation
 
 
@@ -103,7 +103,7 @@ async def get_perceptual_model_citation(model_id: int, session=Depends(get_async
     description="Get the spatial zone type for a perceptual model by ID.",
     response_model=SpatialZoneType | None,
 )
-async def get_perceptual_model_spatial_zone_type(model_id: int, session=Depends(get_async_session)):
+async def get_perceptual_model_spatial_zone_type(model_id: int, session=Depends(get_session)):
     """
     Get the spatial zone type for a perceptual model by ID.
 
@@ -114,8 +114,8 @@ async def get_perceptual_model_spatial_zone_type(model_id: int, session=Depends(
     Returns:
     - The spatial zone type for the perceptual model with the specified ID.
     """
-    model = await session.get(PerceptualModel, model_id)
-    spatial_zone_type = await model.awaitable_attrs.spatial_zone_type
+    model = session.get(PerceptualModel, model_id)
+    spatial_zone_type = model.spatial_zone_type
     return spatial_zone_type
 
 
@@ -124,7 +124,7 @@ async def get_perceptual_model_spatial_zone_type(model_id: int, session=Depends(
     description="Get the temporal zone type for a perceptual model by ID.",
     response_model=TemporalZoneType | None,
 )
-async def get_perceptual_model_temporal_zone_type(model_id: int, session=Depends(get_async_session)):
+async def get_perceptual_model_temporal_zone_type(model_id: int, session=Depends(get_session)):
     """
     Get the temporal zone type for a perceptual model by ID.
 
@@ -135,8 +135,8 @@ async def get_perceptual_model_temporal_zone_type(model_id: int, session=Depends
     Returns:
     - The temporal zone type for the perceptual model with the specified ID.
     """
-    model = await session.get(PerceptualModel, model_id)
-    temporal_zone_type = await model.awaitable_attrs.temporal_zone_type
+    model = session.get(PerceptualModel, model_id)
+    temporal_zone_type = model.temporal_zone_type
     return temporal_zone_type
 
 
@@ -145,7 +145,7 @@ async def get_perceptual_model_temporal_zone_type(model_id: int, session=Depends
     description="Get the model type for a perceptual model by ID.",
     response_model=ModelType | None,
 )
-async def get_perceptual_model_model_type(model_id: int, session=Depends(get_async_session)):
+async def get_perceptual_model_model_type(model_id: int, session=Depends(get_session)):
     """
     Get the model type for a perceptual model by ID.
 
@@ -156,8 +156,8 @@ async def get_perceptual_model_model_type(model_id: int, session=Depends(get_asy
     Returns:
     - The model type for the perceptual model with the specified ID.
     """
-    model = await session.get(PerceptualModel, model_id)
-    model_type = await model.awaitable_attrs.model_type
+    model = session.get(PerceptualModel, model_id)
+    model_type = model.model_type
     return model_type
 
 
@@ -166,7 +166,7 @@ async def get_perceptual_model_model_type(model_id: int, session=Depends(get_asy
     description="Get the process taxonomies for a perceptual model by ID.",
     response_model=List[ProcessTaxonomy] | None,
 )
-async def get_perceptual_model_process_taxonomies(model_id: int, session=Depends(get_async_session)):
+async def get_perceptual_model_process_taxonomies(model_id: int, session=Depends(get_session)):
     """
     Get the process taxonomies for a perceptual model by ID.
 
@@ -177,6 +177,6 @@ async def get_perceptual_model_process_taxonomies(model_id: int, session=Depends
     Returns:
     - The process taxonomies for the perceptual model with the specified ID.
     """
-    model = await session.get(PerceptualModel, model_id)
-    process_taxonomies = await model.awaitable_attrs.process_taxonomies
+    model = session.get(PerceptualModel, model_id)
+    process_taxonomies = model.process_taxonomies
     return process_taxonomies
