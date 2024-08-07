3afeab7221a8a0e559c74896fbbc38b32765c8e8
Devin Cowan <dcowan@cuahsi.org>
2024-08-07 13:53:01 -0400

    remove unecessary env vars

--- api/hydroprocess_db/config/__init__.py
+++ api/hydroprocess_db/config/__init__.py
@@ -10,42 +10,8 @@ load_dotenv()
 class Settings(BaseSettings):
 
     database_url: str
-
-    oauth2_client_id: str
-    oauth2_client_secret: str
-    oauth2_redirect_url: str
-    vite_oauth2_redirect_url: str
     vite_app_api_url: str
     allow_origins: str
-
-    cloud_run: bool = False
-
-    OIDC_BASE_URL: str
-
-    @property
-    def user_info_endpoint(self):
-        return self.OIDC_BASE_URL + "userinfo"
-
-    @property
-    def authorize_endpoint(self):
-        return self.OIDC_BASE_URL + "auth"
-
-    @property
-    def access_token_endpoint(self):
-        return self.OIDC_BASE_URL + "token"
-
-    @property
-    def refresh_token_endpoint(self):
-        # TODO look up refresh token endpoint
-        return self.OIDC_BASE_URL + "token"
-
-    @property
-    def revoke_token_endpoint(self):
-        return self.OIDC_BASE_URL + "revoke"
-
-    @property
-    def revocation_endpoint_auth_method(self):
-        return "client_secret_post"
 
 
 @lru_cache()
