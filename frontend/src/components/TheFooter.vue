194f968e31afaf2c4f5863c1bbdf4c33a9de1c29
MSDrao <msandesh0055@gmail.com>
2024-11-06 01:39:01 -0600

    Added footer related changes

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -2,13 +2,20 @@
   <v-footer v-if="!$route.meta.hideNavigation" app color="navbar" class="d-flex flex-column">
     <div class="d-flex w-100 align-center">
       <div class="text-center w-100">
+        <p class="footerdescription">The work is supported by the National Science Foundation (NSF) under award 2322510: “Synthesizing hydrologic process knowledge through a community database.”</p>
         {{ new Date().getFullYear() }} — <strong>hydroprocess_db</strong>
       </div>
       <v-spacer></v-spacer>
+      <div class="nsfimagecontainer">
+        <img :src="nfsImgUrl" cover  width="100" height="50" class="rounded">
+      </div>
+      <div class="sdsuimagecontainer">
+        <v-img :src="sdsuImgUrl" cover width="8rem" class="rounded"></v-img>
+      </div>
       <v-card href="https://cuahsi.org" class="mx-auto" rel="noopener" target="_blank" subtitle="Powered by" min-width="
         16rem">
         <template v-slot:append>
-          <v-img :src="imgUrl" cover width="8rem"></v-img>
+          <v-img :src="imgUrl" cover width="8rem"></v-img>          
         </template>
       </v-card>
     </div>
@@ -16,7 +23,9 @@
 </template>
 
 <script setup>
-import imgUrl from '@/assets/logo.png'
+import imgUrl from '@/assets/logo.png';
+import sdsuImgUrl from '@/assets/sdsu_logo.png';
+import nfsImgUrl from '@/assets/nsf_logo.png';
 
 </script>
 
@@ -28,5 +37,19 @@ import imgUrl from '@/assets/logo.png'
   img {
     height: 10vh;
   }
+}
+
+.sdsuimagecontainer{
+    margin-right: 8px;
+    height: 48px;
+  }
+
+.nsfimagecontainer{
+  margin-right: -11px;
+  height: 48px;
+}
+
+.footerdescription{
+  font-size: 13px;
 }
 </style>
