bee6420b9c295445bb0b166535e770b75e7f4dd5
MSDrao <msandesh0055@gmail.com>
2024-12-03 10:40:20 -0600

    Updated footer

--- frontend/src/components/TheFooter.vue
+++ frontend/src/components/TheFooter.vue
@@ -1,23 +1,20 @@
 <template>
   <v-footer v-if="!$route.meta.hideNavigation" app color="navbar" class="d-flex flex-column">
-    <div class="d-flex w-100 align-center position-relative">
-      <div class="text-center w-100">
-        <p class="footerdescription">The work is supported by the National Science Foundation (NSF) under award 2322510: “Synthesizing hydrologic process knowledge through a community database.”</p>
-       <div class="footeryearmark">{{ new Date().getFullYear() }} — <strong>hydroprocess_db</strong></div> 
+    <div class="d-flex w-100 position-relative">
+        <div class="nsfimagecontainer float-left">
+          <img :src="nfsImgUrl" cover  width="100" height="50" class="rounded">
+        </div>
+        <div class="sdsuimagecontainer float-left">
+          <v-img :src="sdsuImgUrl" cover width="8rem" class="rounded"></v-img>
+        </div>
+       <div class="poweredbycontainer">
+        <v-card href="https://cuahsi.org" class="mx-auto float-left" rel="noopener" target="_blank" subtitle="Powered by" min-width="
+          16rem">
+          <template v-slot:append>
+            <v-img :src="imgUrl" cover width="8rem"></v-img>          
+          </template>
+        </v-card>
       </div>
-      <v-spacer></v-spacer>
-      <div class="nsfimagecontainer">
-        <img :src="nfsImgUrl" cover  width="100" height="50" class="rounded">
-      </div>
-      <div class="sdsuimagecontainer">
-        <v-img :src="sdsuImgUrl" cover width="8rem" class="rounded"></v-img>
-      </div>
-      <v-card href="https://cuahsi.org" class="mx-auto" rel="noopener" target="_blank" subtitle="Powered by" min-width="
-        16rem">
-        <template v-slot:append>
-          <v-img :src="imgUrl" cover width="8rem"></v-img>          
-        </template>
-      </v-card>
     </div>
   </v-footer>
 </template>
@@ -60,5 +57,10 @@ left: 43%;
 
 .footerdescription{
   margin-top: -28px;
+}
+
+.poweredbycontainer{
+  position: absolute;
+  right: 0;
 }
 </style>
